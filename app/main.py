9a3c4105804994e3964e2edfb6024a2c559f49c8
Ademola Adesina <demolasobaki@gmail.com>
2024-12-18 10:51:05 +0100

    fix cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import convert, user, auth, counsellor, upload
 from . import models
 from .database import engine
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
