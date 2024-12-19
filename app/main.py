66e7d30ae13f8a838fdba5d0184bfaaa03de0872
Ademola Adesina <demolasobaki@gmail.com>
2024-12-19 22:43:18 +0100

    add to cors origin

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from .database import engine
 app = FastAPI()
 
 # origins = ["*"]
-origins = ["https://preview--docu-display-fusion.lovable.app", "http://172.24.70.25:8080", "http://www.ymr-front-end-app.s3-website-us-east-1.amazonaws.com"]
+origins = ["https://preview--docu-display-fusion.lovable.app", "http://172.24.70.25:8080", "http://www.ymr-front-end-app.s3-website-us-east-1.amazonaws.com", "https://id-preview--ba8be7b5-3c14-44c5-92fb-b3906387b4ff.lovable.app"]
 
 app.add_middleware(
     CORSMiddleware,
