70b0e3f2387296cc23fc207ee98885c45c2cc9ce
Ademola Adesina <demolasobaki@gmail.com>
2024-12-27 10:21:10 +0100

    update allowed origin

--- app/main.py
+++ app/main.py
@@ -11,11 +11,10 @@ app = FastAPI()
 # origins = ["*"]
 origins = ["https://preview--docu-display-fusion.lovable.app", 
            "http://172.24.70.25:8080", 
-           "https://id-preview--ba8be7b5-3c14-44c5-92fb-b3906387b4ff.lovable.app", 
            "https://ba8be7b5-3c14-44c5-92fb-b3906387b4ff.lovableproject.com",
            "https://ymr-counselling.vercel.app/",
            "https://www.ymrcounselling.com/"
-           "https://ymrcounselling.com/",
+           "https://ymrcounselling.com/"
         ]
 
 app.add_middleware(
