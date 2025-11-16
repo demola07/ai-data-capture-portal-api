c592be5cd894009549b76c0738f9ef6684d96b70
Ademola Adesina <ademola@cowrywise.com>
2025-11-16 19:36:36 +0100

    trigger pipeline

--- app/main.py
+++ app/main.py
@@ -15,9 +15,6 @@ app = FastAPI()
 
 # origins = ["*"]
 origins = [
-    "https://preview--docu-display-fusion.lovable.app",
-    "http://172.24.70.25:8080",
-    "https://ba8be7b5-3c14-44c5-92fb-b3906387b4ff.lovableproject.com",
     "https://ymr-counselling.vercel.app/",
     "https://www.ymrcounselling.com",
     "https://ymrcounselling.com",
