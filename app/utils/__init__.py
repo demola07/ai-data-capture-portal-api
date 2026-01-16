ce774ed13fd1be53bc6dd86e998e5abecc0659e4
Ademola Adesina <ademola@cowrywise.com>
2026-01-16 12:24:46 +0100

    fix

---
+++ app/utils/__init__.py
@@ -0,0 +1,14 @@
+"""
+Utility functions and helpers
+"""
+from .phone_validator import (
+    format_nigerian_phone,
+    validate_and_format_phones,
+    is_valid_nigerian_phone
+)
+
+__all__ = [
+    'format_nigerian_phone',
+    'validate_and_format_phones',
+    'is_valid_nigerian_phone'
+]
